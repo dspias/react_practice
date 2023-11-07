53d9341245d46288f4c9c80ade863f0e80a548db
Pias Das <23211843+dspias@users.noreply.github.com>
2023-11-07 12:09:13 +0600

    Practice with render props (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import ClickCounter from "./components/ClickCounter";
+import Counter from "./components/Counter";
 import HoverCounter from "./components/HoverCounter";
 
 function App() {
   return (
     <div>
-      <ClickCounter />
-      <HoverCounter />
+      <Counter
+        render={(count, incrementCount) => (
+          <ClickCounter count={count} incrementCount={incrementCount} />
+        )}
+      />
+      <Counter
+        render={(count, incrementCount) => (
+          <HoverCounter count={count} incrementCount={incrementCount} />
+        )}
+      />
     </div>
   );
 }
